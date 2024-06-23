a9609f7ee8d2a82f464953a50405c909305b7cc0
BMajaykumar <bmajaykumar3@gmail.com>
2024-06-23 15:58:43 +0530

    Initial commit with vibration reaction game

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function startGame() {
 
         // Enable vibration if supported
         if (navigator.vibrate) {
-            navigator.vibrate([500]); // Vibrate pattern
+            navigator.vibrate([1000]); // Vibrate pattern
         }
 
         startTime = new Date().getTime();
